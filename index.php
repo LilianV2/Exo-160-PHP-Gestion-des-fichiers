35e1bc14a467fcb168b30b50ef01424da36d82ad
lilia <lilianom59@outlook.fr>
2023-01-16 14:59:18 +0100

    file

--- index.php
+++ index.php
@@ -6,7 +6,10 @@
  * --> Attention à bien fermer votre fichier si vous utilisez fopen()
  */
 // TODO Votre code ici.
-
+$lorem = "larem larem larem larem lorem lorem";
+$file = fopen('lorem.txt', 'a+b');
+file_put_contents('lorem.txt', $lorem);
+fclose($file);
 
 /**
  * 3. Créez un tableau contenant au moins 10 chaînes de caractère au choix.
@@ -15,20 +18,30 @@
  * --> Attention: les chaînes de caractères doivent s'ajouter à la suite du contenu déjà existant, pas d'écrasement.
  */
 // TODO Votre code ici
-
-
+$array = [
+    "L","O","R","E","M","L","O","R","E","M"
+];
+$file = fopen('lorem.txt', 'a+b');
+foreach ($array as $item){
+    fwrite($file,$item);
+}
+fclose($file);
 /**
  * 5. Trouvez une solution pour afficher à l'aide d'un simple echo l'extension du fichier index.php
  */
 // TODO Votre code ici
-
-
+$pathFile = "index.php";
+echo pathinfo($pathFile,PATHINFO_EXTENSION) . "<br><br>";
 
 /**
  * 6. Testez si le fichier 'toto' existe, sil n'existe pas, afficher un texte distant que ce fichier n'existe pas !
  */
 // TODO Votre code ici.
-
+if (file_exists('toto')){
+    echo "Le fichier toto existe";
+}else{
+    echo "Le fichier toto n'existe pas";
+}
 
 /**
  * Super bonus.
@@ -36,3 +49,4 @@
  * Attention, il y a un piège avec les pointeurs, et une manière très simple de procéder... réfléchissez...
  */
 // TODO Votre code ici si vous faites le bonus.
+
